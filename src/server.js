b94ea86b4a97b86fb9045beaf113444f19a76481
Mauricio Villarreal <mauricio.villarreal@outlook.com.ar>
2024-09-14 00:10:33 -0300

    update router react

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser(cookie_parser_secret))
 app.use(cors({
   origin: 'https://lessenza.onrender.com',
-  credentials: true, 
+  credentials: true,
 }));
 
 initSession(app, mongo_url)
